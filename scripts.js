014b8526357a54f359f5018d1386daa03a2ad7b7
bfabradiaz <barby_92_9@hotmail.com>
2021-05-18 01:08:51 -0300

    Primer commit del ejercicio

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button');
+const imgContainer = document.getElementsByClassName('img-container');
+const pName = document.getElementsByTagName('p');
+
+console.log(button);
+console.log(imgContainer);
+console.log(pName);
